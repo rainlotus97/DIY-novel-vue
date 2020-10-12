ba0e2e73e8a1c6281482fda943244ad28f2e1575
rainlotus <68591650+rainlotus97@users.noreply.github.com>
2020-10-12 14:54:47 +0800

    Update aliconfig.js
    写上自己的

--- Loginshare/util/aliconfig.js
+++ Loginshare/util/aliconfig.js
@@ -1,7 +1,7 @@
 module.exports={
     alicloud:{
-        "accessKeyId": 'LTAI4GBZkSSbcLT7D9eYtqas',
-        "accessKeySecret": 'gk1C6HcUAG0iJWkxiikKoBezsWjMZz',
+        "accessKeyId": 'xxxxxxxxx',
+        "accessKeySecret": 'xxxxxxxxxx',
         "endpoint": 'https://dysmsapi.aliyuncs.com',
         "apiVersion": '2017-05-25'
     }
